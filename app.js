c5a6268b83f6e6cf286f8aa983f3681a4205ad9a
Devapujyula <sdevapujyula@gmail.com>
2024-07-28 08:47:16 +0530

    episode-2

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world... Welcome to ReactJS"
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-console.log(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 Tag"),
+    React.createElement("h2", {}, "I am an H2 Tag"),
+  ]),
+]);
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
