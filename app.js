308e907f155c11a9447a058073c68ae3ed287ab2
Devapujyula <sdevapujyula@gmail.com>
2024-08-03 14:38:28 +0530

    episode-4

--- app.js
+++ app.js
@@ -1,54 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-// React.createElement => ReactElement (Object) =>  HTMLElement(render)
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React course..."
-);
-
-// JSX Comes into play
-// JSX - is not the HTML it's is HTML or XML  like syntax
-// JSX => Babel transpile it to React.createElement => ReactElement_JS Object =>HTMLElement(render)
-// JSX- transpile before it reaches the JS Engine - Parcel - Babel
-const jsxHeading = (
-  <h1 id="heading" className="head">
-    Namaste React using JSX
-  </h1>
-);
-
-// React Component
-// Class Based Components - OLD
-// Functional Components - NEW
-
-// React Functional Component
-
-const elem = <span>ReactElement</span>;
-
-const title = (
-  <h1 className="head">
-    {elem}
-    Using Component
-  </h1>
-);
-
-const number = 10000;
-
-// Component Composition
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      <h1 className="header">Namaste React Functional Component🚀</h1>
-      <h2>{number}</h2>
-      <h3>{title}</h3>
+    <div className="header">
+      <div className="logo-containers">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzWG3-5wfqETFXtPOAMOSl6lXpxGjG3fGvuQ&s"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6Wc-hevy-yxW0pVomqf2_T6_w4EOLiSl22w&s"
+      />
+      <h3>Bawarchi</h3>
+      <h4>Biryani, South Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 mints</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-root.render(jsxHeading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
